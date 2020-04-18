c1de3fc14b8b4011091b746805207e483e40d512
charlesmartinreed <charlesmartinreed@icloud.com>
2020-04-17 19:35:59 -0500

    Init. Finish challenge 1.

---
+++ main.js
@@ -0,0 +1,12 @@
+var product = "Socks"
+
+// create a new Vue instance
+
+var app = new Vue({
+    // initialization options go here
+    el: "#app", // connect to the #app element on our DOM
+    data: {
+        product: 'Socks',
+        description: "So luxurious, so fashionable. Go ahead and buy them - it's OK to be extra."
+    }
+})
