aa6de18a877e920d865183d706492cc81fa9f67a
charlesmartinreed <charlesmartinreed@icloud.com>
2020-04-17 19:49:22 -0500

    Complete challenge #2.

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-var product = "Socks"
+var product = "Socks";
 
 // create a new Vue instance
 
 var app = new Vue({
-    // initialization options go here
-    el: "#app", // connect to the #app element on our DOM
-    data: {
-        product: 'Socks',
-        description: "So luxurious, so fashionable. Go ahead and buy them - it's OK to be extra."
-    }
-})
+  // initialization options go here
+  el: "#app", // connect to the #app element on our DOM
+  data: {
+    title: "Socks",
+    description:
+      "So luxurious, so fashionable. Go ahead and buy them - it's OK to be extra.",
+    image: "./assets/images/vmSocks-green-onWhite.jpg",
+    link: "https://hotsocks.biz",
+  },
+});
