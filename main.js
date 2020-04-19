5ed598893f1c73c4e9fceff95564a8dbf3f30829
charlesmartinreed <charlesmartinreed@icloud.com>
2020-04-18 21:14:50 -0500

    Complete Challenge #6.

--- main.js
+++ main.js
@@ -6,13 +6,12 @@ var app = new Vue({
   // initialization options go here
   el: "#app", // connect to the #app element on our DOM
   data: {
-    title: "Socks",
+    brand: 'Millionairess',
+    product: "Socks",
     description:
       "So luxurious, so fashionable. Go ahead and buy them - it's OK to be extra.",
     link: "https://hotsocks.biz",
     inventory: 5,
-    onSale: false,
-    inStock: true,
     details: [
       "80% cotton",
       "20% polyester",
@@ -20,17 +19,21 @@ var app = new Vue({
       "Gender neutral",
     ],
     cart: 0,
-    image: "",
+    selectedVariant: 0,
     variants: [
       {
         variantId: 2234,
         variantColor: "green",
         variantImage: "./assets/images/vmSocks-green-onWhite.jpg",
+        variantQuantity: 10,
+        onSale: true
       },
       {
         variantId: 2235,
         variantColor: "blue",
         variantImage: "./assets/images/vmSocks-blue-onWhite.jpg",
+        variantQuantity: 0,
+        onSale: false
       },
     ],
     sizes: [
@@ -53,8 +56,24 @@ var app = new Vue({
         return;
       }
     },
-    updateProduct(prop) {
-      this.image = prop;
+    updateProduct(index) {
+      this.selectedVariant = index;
     },
+  },
+  // results of a computed property are CACHED, which is what makes this more efficient than using a method when the operation in the computed property is expensive
+  computed: {
+    title: function() {
+      return `${this.brand} ${this.product}`;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity > 0;
+    },
+    onSale() {
+      return this.variants[this.selectedVariant].onSale ? `These incredibly dope ${this.brand} ${this.product} are currently on sale!` : null
+  
+    }
   },
 });
